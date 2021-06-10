8307f65fd93d6eb7ef931559e5e2024d2408fed3
william villani stiehler <william.stiehler@olist.com>
2021-06-10 09:01:37 -0300

    upload front

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
     'seller',
     'marketplace'
 ]
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from marketplace import views
 from marketplace.views import marketplaces, marketplacesForm, createMarketplaces, editMarketplace, updateMarketplace, deleteMarketplace, viewMarketplace
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='base'),
     path('', include('seller.urls')),
-    #path('', include('marketplace.urls'))
+    path('', include('marketplace.urls')),
+    path('admin/', admin.site.urls),
 ]
